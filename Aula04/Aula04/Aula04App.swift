83ebfc799d4d44c52dea9ac76ea5aca6806fbbfc
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-05-22 18:44:56 -0300

    aula 4 spotify clone

---
+++ Aula04/Aula04/Aula04App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula04App.swift
+//  Aula04
+//
+//  Created by Student22 on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
